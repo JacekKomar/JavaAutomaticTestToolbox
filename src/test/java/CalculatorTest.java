36d3aeb32d7be4725253d5d3c2b5996c549f169c
JacekKomar <jacek.komar2000@gmail.com>
2023-02-02 19:00:00 +0100

    Add before method to DayScheduleTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CalculatorTest extends Main{
@@ -10,7 +11,7 @@ public class CalculatorTest extends Main{
     public void deleteScore(){
         driver.findElement(By.xpath("(//button[normalize-space()='AC'])")).click();
     }
-    @AfterMethod
+    @BeforeMethod
     public void informationAboutCalculating(){
         System.out.println("Kolejne Obliczenie:");
     }
--- src/test/java/DayScheduleTest.java
+++ src/test/java/DayScheduleTest.java
@@ -1,2 +1,41 @@
-public class DayScheduleTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DayScheduleTest extends Main{
+
+    @BeforeMethod
+        public void addTaskConsoleInfo(){
+            System.out.println("Dodano zadanie:");
+        }
+
+
+    @Test
+    public void findDaySchedulePage() throws InterruptedException {
+        driver.findElement(By.xpath("(//div[@class='ChooseSite col-sm p-4 mr-1 border border-dark rounded brightened'])[2]")).click();
+        Thread.sleep(2000);
+
+    }
+
+    @Test
+    public void addFirstTask() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement addTask = driver.findElement(By.xpath("//input[@placeholder='Wpisz swoje zadanie']"));
+        addTask.click();
+        addTask.sendKeys("Napisanie Cv.");
+        driver.findElement(By.xpath("(//h3[normalize-space()='Dodaj swoje zadania'])")).click();
+        Thread.sleep(2000);
+}
+
+
+
+
+
+
+
+
+
+
 }
