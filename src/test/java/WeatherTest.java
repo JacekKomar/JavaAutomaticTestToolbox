88e47d475fe3daf5ca919b26e202a98700bb67b6
JacekKomar <jacek.komar2000@gmail.com>
2023-02-02 14:40:39 +0100

    Saved information about weather in console

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -1,6 +1,10 @@
+import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v106.css.model.Value;
 import org.testng.annotations.Test;
 public class WeatherTest {
 
@@ -9,11 +13,20 @@ public class WeatherTest {
 
 
     @Test
-    public void findWeatherPage() {
+    public void findWeatherPage() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://jacekkomar.github.io/toolbox/#/");
         driver.findElement(By.xpath("//div[@class='ChooseSite col-sm mr-1 p-4 border border-dark rounded brightened']")).click();
         driver.findElement(By.xpath("(//button[contains(@class,'rounded border border-dark brightenedDark')])[2]")).click();
+        WebElement cityName = driver.findElement(By.xpath("(//input[@placeholder=' Wpisz nazwę miejscowości'])"));
+        cityName.sendKeys("Warszawa");
+        System.out.println("Miasto to:" + " " +  cityName.getAttribute ( "value"));
+        cityName.sendKeys(Keys.ENTER);
+        Thread.sleep(5000);
+    WebElement actualWeather = driver.findElement(By.xpath("//p[contains(.,'°C')]"));
+        System.out.println("Temperatura to:" + " " +  actualWeather.getAttribute ( "value"));
+
+//     driver.quit();
     }
 }
